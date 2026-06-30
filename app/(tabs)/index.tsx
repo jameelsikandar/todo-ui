1b1917fe402a3387b37d2fb572f205cdb3822b0f
jameelsikandar <jameelsikandarr@gmail.com>
2026-07-01 03:03:54 +0500

    tabs reanm

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,8 +3,15 @@ import { Text, View } from "react-native";
 export default function HomeScreen() {
   return (
     <View style={{ flex: 1, backgroundColor: "#05243E", padding: 20 }}>
-      <Text style={{ color: "#fff", fontSize: 28, fontWeight: "bold" }}>
-        Good Morning! 👋
+      <Text
+        style={{
+          color: "#fff",
+          fontSize: 28,
+          fontWeight: "bold",
+          marginTop: 20,
+        }}
+      >
+        Home Page
       </Text>
       <Text style={{ color: "#aaa", marginTop: 10 }}>
         Lets make today productive
